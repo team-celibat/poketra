b3a6f1fbece319e515acaab40d9a0d62982998f3
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-06 22:27:13 +0300

    first script

--- lib/app/poketra.dart
+++ lib/app/poketra.dart
@@ -5,9 +5,11 @@ class PoketraApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-
+        appBar: AppBar(
+          title: Title(color: Colors.blue, child: const Text("Poketra"),)
+        ),
       ),
     );
   }
