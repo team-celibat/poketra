888bbc146c1ac9165770b9345929fbfbc7ba9602
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-09 23:05:05 +0300

    feat : add external injection

---
+++ lib/core/config/injection.dart
@@ -0,0 +1,14 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init()async {
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => Connectivity());
+}
