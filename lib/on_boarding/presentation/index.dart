4e9a6df8d5024122ea75387afe77a3f1c6c32a7c
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2024-05-10 09:11:19 +0300

    feat : add on boarding page section

---
+++ lib/on_boarding/presentation/index.dart
@@ -0,0 +1,5 @@
+//view
+export 'views/on_boarding.dart';
+
+//manager
+export 'manager/example_bloc.dart';
---
+++ lib/on_boarding/presentation/widgets/on_boarding_bottom_navigation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingBottomNavigation extends StatelessWidget {
+  const OnBoardingBottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
