735362bfab521c69dbaa0d253fc021f25c6e8c21
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-05-11 21:41:56 +0300

    [FIX] initialization bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:poketra/app/poketra.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const PoketraApp());
 }
 
--- lib/on_boarding/presentation/widgets/on_boarding_top_navigation.dart
+++ lib/on_boarding/presentation/widgets/on_boarding_top_navigation.dart
@@ -6,17 +6,24 @@ class OnBoardingTopNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(0),
-      child: const Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(".Poketra",
-            style: TextStyle(color: Colors.black87,fontWeight: FontWeight.bold,fontSize: 35),
-          ),
-          TextButton(onPressed: null, child: Text("skip",style: TextStyle(color: Colors.black54,fontSize: 20),))
-
-        ],
-      )
-    );
+        padding: const EdgeInsets.all(0),
+        child: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              ".Poketra",
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35),
+            ),
+            TextButton(
+                onPressed: null,
+                child: Text(
+                  "skip",
+                  style: TextStyle(color: Colors.black54, fontSize: 20),
+                ))
+          ],
+        ));
   }
 }
