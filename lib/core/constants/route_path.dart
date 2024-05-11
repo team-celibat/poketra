c05daed5a3bd7013b8f4b972e758e240a4e33293
TiaDev7474 <nomenjanaharyriry@gmail.com>
2024-05-11 22:49:07 +0300

    [FIX] change home path to /

--- lib/core/constants/route_path.dart
+++ lib/core/constants/route_path.dart
@@ -1,11 +1,11 @@
 
 
 abstract class RoutePath {
-  static String home = '/home';
+  static String home = '/';
   static String onboarding= '/onboarding';
-  static String auth = '/auth/';
+  static String auth = '/auth';
   static String login = 'login';
-  static String signup = '/signup';
+  static String signup = 'signup';
   static String statistics = '/statistics';
   static String goals = '/goals';
   static String profile = '/profile';
